0b08d081ab104e26c5acca1bbb7dba420cff3a24
yorukai <33402703+yorukai@users.noreply.github.com>
2025-10-03 17:10:45 +0200

    Added tailwindcss/oxide

--- src/app/(frontend)/[locale]/posts/page/[pageNumber]/page.tsx
+++ src/app/(frontend)/[locale]/posts/page/[pageNumber]/page.tsx
@@ -10,8 +10,6 @@ import { notFound } from 'next/navigation'
 import { getPayload, TypedLocale } from 'payload'
 import PageClient from './page.client'
 
-export const revalidate = 600
-
 type Args = {
   params: Promise<{
     locale: TypedLocale
