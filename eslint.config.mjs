2f8803247be0cf3266f1ea3a5cd9b8628a8ce9cb
yorukai <33402703+yorukai@users.noreply.github.com>
2025-10-03 16:29:06 +0200

    Updated eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,16 @@ const compat = new FlatCompat({
 })
 
 const eslintConfig = [
+  {
+    ignores: [
+      'node_modules/**',
+      '.next/**',
+      '.pnpm-store/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+    ],
+  },
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     rules: {
